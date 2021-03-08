b2777d64ffc7a03a62ecdf5fc4b4dd3ecfe429d5
pextech <mcstain1639@gmail.com>
2021-03-08 10:58:04 -0800

    inital deploy to github

---
+++ Controllers/MoviesController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vividly.Models;
+using Vividly.ViewModels.
+
+namespace Vividly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies/Random
+        public ActionResult Random()
+        {
+            var movie = new movie() { Name = "Shrek!" };
+            var customers = new List<Customer>
+            {
+                new Customer { Name= "Customer 1"},
+                new Customer { Name= "Customer 1"},
+            };
+
+            var viewModel = new RandomMoviewViewModel();
+
+            return View(movie);
+        }
+    }
+}
---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Vividly.MvcApplication" Language="C#" %>
